215f22018a1d988ac87e26880112ae25bf645a09
phoboz <alexuspol@hotmail.com>
2013-10-15 22:07:00 +0200

    Cleaned up qFifoLib

--- include/util/qFifoLib.h
+++ include/util/qFifoLib.h
@@ -1,22 +1,22 @@
 /******************************************************************************
-*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-*
-*   This file is part of Real VMX.
-*   Copyright (C) 2008 - 2009 Surplus Users Ham Society
-*
-*   Real VMX is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU Lesser General Public License as published by
-*   the Free Software Foundation, either version 2.1 of the License, or
-*   (at your option) any later version.
-*
-*   Real VMX is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU Lesser General Public License for more details.
-*
-*   You should have received a copy of the GNU Lesser General Public License
-*   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
-******************************************************************************/
+ *   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *   This file is part of Real VMX.
+ *   Copyright (C) 2013 Surplus Users Ham Society
+ *
+ *   Real VMX is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Real VMX is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* qFifoLib.h - Fifo queue */
 
@@ -32,29 +32,34 @@
 extern "C" {
 #endif
 
+#include <vmx.h>
 #include <util/dllLib.h>
 #include <util/qLib.h>
 
-#define FIFO_KEY_HEAD	-1
-#define FIFO_KEY_TAIL	0
+#define FIFO_KEY_HEAD          -1
+#define FIFO_KEY_TAIL           0
 
 /* typedefs */
 
-typedef union {            /* Explicitly show that Q_FIFO_NODE */
+typedef union              /* Explicitly show that Q_FIFO_NODE */
+{
     Q_NODE  qNode;         /* overlays Q_NODE. */
-    struct {
+    struct
+    {
         DL_NODE  node;
     } qFifo;
 } Q_FIFO_NODE;
 
-typedef union {            /* Explicitly show that Q_FIFO_HEAD */
+typedef union              /* Explicitly show that Q_FIFO_HEAD */
+{
     Q_HEAD  qHead;         /* overlays Q_HEAD. */
-    struct {
+    struct
+    {
         DL_LIST  head;
     } qFifo;
 } Q_FIFO_HEAD;
 
-extern Q_CLASS_ID qFifoClassId;
+IMPORT Q_CLASS_ID qFifoClassId;
 
 #ifdef __cplusplus
 }
--- src/util/qFifoLib.c
+++ src/util/qFifoLib.c
@@ -1,22 +1,22 @@
 /******************************************************************************
-*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-*
-*   This file is part of Real VMX.
-*   Copyright (C) 2008 - 2009 Surplus Users Ham Society
-*
-*   Real VMX is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU Lesser General Public License as published by
-*   the Free Software Foundation, either version 2.1 of the License, or
-*   (at your option) any later version.
-*
-*   Real VMX is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU Lesser General Public License for more details.
-*
-*   You should have received a copy of the GNU Lesser General Public License
-*   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
-******************************************************************************/
+ *   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *   This file is part of Real VMX.
+ *   Copyright (C) 2013 Surplus Users Ham Society
+ *
+ *   Real VMX is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Real VMX is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* qFifoLib.c - Fifo queue */
 
@@ -25,199 +25,275 @@
 #include <util/qFifoLib.h>
 
 /* Forward declarations */
-LOCAL Q_FIFO_HEAD* qFifoCreate(void);
-LOCAL STATUS qFifoInit(Q_FIFO_HEAD *pQFifoHead);
-LOCAL STATUS qFifoDestroy(Q_FIFO_HEAD *pQFifoHead);
-LOCAL void qFifoPut(Q_FIFO_HEAD *pQFifoHead,
-		    Q_FIFO_NODE *pQFifoNode,
-		    int key);
-LOCAL Q_FIFO_NODE* qFifoGet(Q_FIFO_HEAD *pQFifoHead);
-LOCAL STATUS qFifoRemove(Q_FIFO_HEAD *pQFifoHead,
-			 Q_FIFO_NODE *pQFifoNode);
-LOCAL int qFifoInfo(Q_FIFO_HEAD *pQFifoHead, int nodeArray[], int maxNodes);
-LOCAL Q_FIFO_NODE* qFifoEach(Q_FIFO_HEAD *pQFifoHead, FUNCPTR func, int arg);
-LOCAL void qFifoNull(void);
+LOCAL Q_FIFO_HEAD* qFifoCreate(
+    void
+    );
+
+LOCAL STATUS qFifoInit(
+    Q_FIFO_HEAD *pQFifoHead
+    );
+
+LOCAL STATUS qFifoDestroy(
+    Q_FIFO_HEAD *pQFifoHead
+    );
+
+LOCAL void qFifoPut(
+    Q_FIFO_HEAD *pQFifoHead,
+    Q_FIFO_NODE *pQFifoNode,
+    int key
+    );
+
+LOCAL Q_FIFO_NODE* qFifoGet(
+    Q_FIFO_HEAD *pQFifoHead
+    );
+
+LOCAL STATUS qFifoRemove(
+    Q_FIFO_HEAD *pQFifoHead,
+    Q_FIFO_NODE *pQFifoNode
+    );
+
+LOCAL int qFifoInfo(
+    Q_FIFO_HEAD* pQFifoHead,
+    int nodeArray[],
+    int maxNodes
+    );
+
+LOCAL Q_FIFO_NODE* qFifoEach(
+    Q_FIFO_HEAD *pQFifoHead,
+    FUNCPTR func,int arg
+    );
+
+LOCAL void qFifoNull(
+    void
+    );
 
 /* Locals */
 LOCAL Q_CLASS qFifoClass =
 {
-  (FUNCPTR) qFifoCreate,
-  (FUNCPTR) qFifoInit,
-  (FUNCPTR) qFifoDestroy,
-  (FUNCPTR) qFifoNull,
-  (FUNCPTR) qFifoPut,
-  (FUNCPTR) qFifoGet,
-  (FUNCPTR) qFifoRemove,
-  (FUNCPTR) qFifoNull,
-  (FUNCPTR) qFifoNull,
-  (FUNCPTR) qFifoNull,
-  (FUNCPTR) qFifoNull,
-  (FUNCPTR) qFifoNull,
-  (FUNCPTR) qFifoInfo,
-  (FUNCPTR) qFifoEach,
-  &qFifoClass
+    (FUNCPTR) qFifoCreate,
+    (FUNCPTR) qFifoInit,
+    (FUNCPTR) qFifoDestroy,
+    (FUNCPTR) qFifoNull,
+    (FUNCPTR) qFifoPut,
+    (FUNCPTR) qFifoGet,
+    (FUNCPTR) qFifoRemove,
+    (FUNCPTR) qFifoNull,
+    (FUNCPTR) qFifoNull,
+    (FUNCPTR) qFifoNull,
+    (FUNCPTR) qFifoNull,
+    (FUNCPTR) qFifoNull,
+    (FUNCPTR) qFifoInfo,
+    (FUNCPTR) qFifoEach,
+    &qFifoClass
 };
 
 Q_CLASS_ID qFifoClassId = &qFifoClass;
 
-/*******************************************************************************
-* qFifoCreate - Create queue
-*
-* RETURNS: Queue head pointer
-*******************************************************************************/
+/******************************************************************************
+ * qFifoCreate - Create queue
+ *
+ * RETURNS: Queue head pointer or NULL
+ */
 
-LOCAL Q_FIFO_HEAD* qFifoCreate(void)
+LOCAL Q_FIFO_HEAD* qFifoCreate(
+    void
+    )
 {
-  Q_FIFO_HEAD *pQFifoHead;
+    Q_FIFO_HEAD *pQFifoHead;
 
-  /* Allocate memory for struct */
-  pQFifoHead = malloc( sizeof(Q_FIFO_HEAD) );
+    /* Allocate memory for struct */
+    pQFifoHead = malloc(sizeof(Q_FIFO_HEAD));
 
-  if (pQFifoHead == NULL) {
-    return(NULL);
+    if (pQFifoHead != NULL)
+    {
+        /* Call initializer below */
+        if (qFifoInit(pQFifoHead) != OK)
+        {
+            free(pQFifoHead);
+            pQFifoHead = NULL;
+        }
   }
 
-  /* Call initializer below */
-  if (qFifoInit(pQFifoHead) != OK) {
-    return(NULL);
-  }
-
-  return(pQFifoHead);
+  return pQFifoHead;
 }
 
-/*******************************************************************************
-* qFifoInit - Initialize queue
-*
-* RETURNS: OK or ERROR
-*******************************************************************************/
+/******************************************************************************
+ * qFifoInit - Initialize queue
+ *
+ * RETURNS: OK
+ */
 
-LOCAL STATUS qFifoInit(Q_FIFO_HEAD *pQFifoHead)
+LOCAL STATUS qFifoInit(
+    Q_FIFO_HEAD *pQFifoHead
+    )
 {
 
-  dllInit(&pQFifoHead->qFifo.head);
+    dllInit(&pQFifoHead->qFifo.head);
 
-  return(OK);
+    return OK;
 }
 
-/*******************************************************************************
-* qFifoDestroy - Deallocate queue
-*
-* RETURNS: OK or ERROR
-*******************************************************************************/
+/******************************************************************************
+ * qFifoDestroy - Deallocate queue
+ *
+ * RETURNS: OK
+ */
 
-LOCAL STATUS qFifoDestroy(Q_FIFO_HEAD *pQFifoHead)
+LOCAL STATUS qFifoDestroy(
+    Q_FIFO_HEAD *pQFifoHead
+    )
 {
-  /* Free memory */
-  free(pQFifoHead);
+    /* Free memory */
+    free(pQFifoHead);
 
-  return(OK);
+    return OK;
 }
 
-/*******************************************************************************
-* qFifoPut - Put a node on the queue
-*
-* RETURNS: OK or ERROR
-*******************************************************************************/
+/******************************************************************************
+ * qFifoPut - Put a node on the queue
+ *
+ * RETURNS: OK or ERROR
+ */
 
-LOCAL void qFifoPut(Q_FIFO_HEAD *pQFifoHead,
-		    Q_FIFO_NODE *pQFifoNode,
-		    int key)
+LOCAL void qFifoPut(
+    Q_FIFO_HEAD *pQFifoHead,
+    Q_FIFO_NODE *pQFifoNode,
+    int key
+    )
 {
-  if (key == FIFO_KEY_HEAD)
-    dllInsert (&pQFifoHead->qFifo.head, (DL_NODE *) NULL,
-	       &pQFifoNode->qFifo.node);
-  else
-    dllAdd(&pQFifoHead->qFifo.head, &pQFifoNode->qFifo.node);
+    if (key == FIFO_KEY_HEAD)
+    {
+        dllInsert(
+            &pQFifoHead->qFifo.head,
+            (DL_NODE *) NULL,
+            &pQFifoNode->qFifo.node
+            );
+    }
+    else
+    {
+        dllAdd(&pQFifoHead->qFifo.head, &pQFifoNode->qFifo.node);
+    }
 }
 
-/*******************************************************************************
-* qFifoGet - Get and remove a node from queue
-*
-* RETURNS: Pointer to node
-*******************************************************************************/
+/******************************************************************************
+ * qFifoGet - Get and remove a node from queue
+ *
+ * RETURNS: Pointer to node
+ */
 
-LOCAL Q_FIFO_NODE* qFifoGet(Q_FIFO_HEAD *pQFifoHead)
+LOCAL Q_FIFO_NODE* qFifoGet(
+    Q_FIFO_HEAD *pQFifoHead
+    )
 {
-  if (DLL_EMPTY(&pQFifoHead->qFifo.head))
-    return(NULL);
+    Q_FIFO_NODE *pQFifoNode;
 
-  return ((Q_FIFO_NODE *) dllGet(&pQFifoHead->qFifo.head));
+    if (DLL_EMPTY(&pQFifoHead->qFifo.head))
+    {
+        pQFifoNode = NULL;
+    }
+    else
+    {
+        pQFifoNode = ((Q_FIFO_NODE *) dllGet(&pQFifoHead->qFifo.head));
+    }
+
+    return pQFifoNode;
 }
 
-/*******************************************************************************
-* qFifoListRemove - Remove node
-*
-* RETURNS: OK or ERROR
-*******************************************************************************/
+/******************************************************************************
+ * qFifoListRemove - Remove node
+ *
+ * RETURNS: OK
+ */
 
-LOCAL STATUS qFifoRemove(Q_FIFO_HEAD *pQFifoHead,
-			 Q_FIFO_NODE *pQFifoNode)
+LOCAL STATUS qFifoRemove(
+    Q_FIFO_HEAD *pQFifoHead,
+    Q_FIFO_NODE *pQFifoNode
+    )
 {
-  dllRemove(&pQFifoHead->qFifo.head, &pQFifoNode->qFifo.node);
+    dllRemove(&pQFifoHead->qFifo.head, &pQFifoNode->qFifo.node);
 
-  return(OK);
+    return OK;
 }
 
-/*******************************************************************************
-* qFifoInfo - Get info about queue
-*
-* RETURNS: Number of infos read
-*******************************************************************************/
+/******************************************************************************
+ * qFifoInfo - Get info about queue
+ *
+ * RETURNS: Number of infos read
+ */
 
-LOCAL int qFifoInfo(Q_FIFO_HEAD *pQFifoHead, int nodeArray[], int maxNodes)
+LOCAL int qFifoInfo(
+    Q_FIFO_HEAD *pQFifoHead,
+    int nodeArray[],
+    int maxNodes
+    )
 {
-  DL_NODE *pNode;
-  int *pElement;
+    DL_NODE *pNode;
+    int *pElement;
+    int count;
 
-  /* If null array just return number of nodes */
-  if (nodeArray == NULL)
-    return dllCount((DL_LIST *) pQFifoHead);
+    /* If null array just return number of nodes */
+    if (nodeArray == NULL)
+    {
+        count = dllCount((DL_LIST *) pQFifoHead);
+    }
+    else
+    {
+        /* Get first node */
+        pNode = (DL_NODE *) DLL_HEAD((DL_LIST *) pQFifoHead);
 
-  /* Get first node */
-  pNode = (DL_NODE *) DLL_HEAD( (DL_LIST *) pQFifoHead);
+        /* Set element pointer */
+        pElement = nodeArray;
 
-  /* Set element pointer */
-  pElement = nodeArray;
+        /* While node in list or maxNodex GE 0*/
+        while ((pNode != NULL) && (--maxNodes >= 0))
+        {
+            *(pElement++) = (int) pNode;
+            pNode = DLL_NEXT(pNode);
+        }
 
-  /* While node in list or maxNodex GE 0*/
-  while ( (pNode != NULL) && (--maxNodes >= 0) ) {
+        /* Return numbder of nodes read */
+        count = pElement - nodeArray;
+    }
 
-    *(pElement++) = (int) pNode;
-    pNode = DLL_NEXT(pNode);
-
-  }
-
-  /* Return numbder of nodes read */
-  return (pElement - nodeArray);
+    return count;
 }
 
-/*******************************************************************************
-* qFifoEach - Run function for each node in queue
-*
-* RETURNS: NULL or node where it ended
-*******************************************************************************/
+/******************************************************************************
+ * qFifoEach - Run function for each node in queue
+ *
+ * RETURNS: NULL or node where it ended
+ */
 
-LOCAL Q_FIFO_NODE* qFifoEach(Q_FIFO_HEAD *pQFifoHead, FUNCPTR func, int arg)
+LOCAL Q_FIFO_NODE* qFifoEach(
+    Q_FIFO_HEAD *pQFifoHead,
+    FUNCPTR func,
+    int arg
+    )
 {
-  DL_NODE *pNode;
+    DL_NODE *pNode;
 
-  /* Get first node */
-  pNode = (DL_NODE *) DLL_HEAD( (DL_LIST *) pQFifoHead);
+    /* Get first node */
+    pNode = (DL_NODE *) DLL_HEAD( (DL_LIST *) pQFifoHead);
 
-  /* While node in list or nonzero return from function */
-  while ( (pNode != NULL) && ((* func) (pNode, arg)) )
-    pNode = DLL_NEXT(pNode);
+    /* While node in list or nonzero return from function */
+    while ((pNode != NULL) && ((* func)(pNode, arg)))
+    {
+        pNode = DLL_NEXT(pNode);
+    }
 
-  return (Q_FIFO_NODE *) pNode;
+    return (Q_FIFO_NODE *) pNode;
 }
 
-/*******************************************************************************
-* qFifoNull - Null method
-*
-* RETURNS: N/A
-*******************************************************************************/
+/******************************************************************************
+ * qFifoNull - Null method
+ *
+ * RETURNS: N/A
+ */
 
-LOCAL void qFifoNull(void)
+LOCAL void qFifoNull(
+    void
+    )
 {
-  return;
+    return;
 }
+
