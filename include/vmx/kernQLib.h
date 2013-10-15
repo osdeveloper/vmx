457bb02b88a898b2d43be195953a806db5804470
phoboz <alexuspol@hotmail.com>
2013-10-15 21:01:13 +0200

    Worked on kernQLib

--- include/vmx/kernQLib.h
+++ include/vmx/kernQLib.h
@@ -1,22 +1,22 @@
 /******************************************************************************
-*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-*
-*   This file is part of Real VMX.
-*   Copyright (C) 2008 Surplus Users Ham Society
-*
-*   Real VMX is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   Real VMX is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
-******************************************************************************/
+ *   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *   This file is part of Real VMX.
+ *   Copyright (C) 2013 Surplus Users Ham Society
+ *
+ *   Real VMX is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Real VMX is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* kernQLib.h - Kernel work queue library */
 
@@ -30,26 +30,80 @@ extern "C" {
 #endif
 
 /* Struct */
-typedef struct {
-  FUNCPTR func;
-  int numArgs;
-  ARG arg1;
-  ARG arg2;
+typedef struct
+{
+    FUNCPTR func;    /* ptr to function that was added */
+    int     numArgs; /* # of arguments to added item */
+    ARG     arg1;    /* 1st argument of added function (if applicable) */
+    ARG     arg2;    /* 2nd argument of added function (if applicable) */
 } KERN_JOB;
 
 /* Globals */
-extern volatile unsigned       kernQReadIndex;
-extern volatile unsigned       kernQWriteIndex;
-extern volatile BOOL           kernQEmpty;
-extern KERN_JOB                kernJobs[];
+extern volatile BOOL kernQEmpty;
 
 /* Functions */
-extern STATUS kernQLibInit(void);
-extern void kernQAdd0(FUNCPTR func);
-extern void kernQAdd1(FUNCPTR func, ARG arg1);
-extern void kernQAdd2(FUNCPTR func, ARG arg1, ARG arg2);
-extern void kernQDoWork(void);
-extern void kernQPanic(void);
+/******************************************************************************
+ * kernQLibInit - Initialize kernel work queue
+ *
+ * RETURNS: OK
+ */
+
+STATUS kernQLibInit(
+    void
+    );
+
+/******************************************************************************
+ * kernQAdd0 - Add a kernel job with no aguments
+ *
+ * RETURNS: N/A
+ */
+
+void kernQAdd0(
+    FUNCPTR func    /* ptr to deferred function */
+    );
+
+/******************************************************************************
+ * kernQAdd1 - Add a kernel job with one agument
+ *
+ * RETURNS: N/A
+ */
+
+void kernQAdd1(
+    FUNCPTR func,     /* ptr to function to add */
+    ARG arg1          /* 1st argument to deferred function */
+    );
+
+/******************************************************************************
+ * kernQAdd2 - Add a kernel job with two aguments
+ *
+ * RETURNS: N/A
+ */
+
+void kernQAdd2(
+    FUNCPTR func,     /* ptr to function to add */
+    ARG arg1,         /* 1st argument to deferred function */
+    ARG arg2          /* 2nd argument to deferred function */
+    );
+
+/******************************************************************************
+ * kernQDoWork - Do job on the kernel queue
+ *
+ * RETURNS: N/A
+ */
+
+void kernQDoWork(
+    void
+    );
+
+/******************************************************************************
+ * kernQPanic - Fatal error in queue
+ *
+ * RETURNS: N/A
+ */
+
+void kernQPanic(
+    void
+    );
 
 #ifdef __cplusplus
 }
--- src/vmx/kernQLib.c
+++ src/vmx/kernQLib.c
@@ -1,201 +1,247 @@
 /******************************************************************************
-*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-*
-*   This file is part of Real VMX.
-*   Copyright (C) 2008 Surplus Users Ham Society
-*
-*   Real VMX is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   Real VMX is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
-******************************************************************************/
+ *   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *   This file is part of Real VMX.
+ *   Copyright (C) 2013 Surplus Users Ham Society
+ *
+ *   Real VMX is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Real VMX is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* kernQLib.c - Kernel work queue library */
+
+/*
+The kernel queue is implemented as a ring buffer.
+*/
+
+/* includes */
 
 #include <vmx.h>
 #include <arch/intArchLib.h>
 #include <arch/sysArchLib.h>
-#include <vmx/private/kernLibP.h>
+#include <vmx/errnoLib.h>
 #include <vmx/kernQLib.h>
 
-/* Globals */
-volatile unsigned	kernQReadIndex;
-volatile unsigned	kernQWriteIndex;
-volatile BOOL		kernQEmpty;
-KERN_JOB		kernJobs[255];
+/* defines */
+
+#define MAX_KERN_Q_JOBS        256    /* Always a power of two. */
+
+/* globals */
+volatile BOOL kernQEmpty;
+
+/* locals */
+LOCAL volatile unsigned kernQReadIndex;
+LOCAL volatile unsigned kernQWriteIndex;
+LOCAL KERN_JOB          kernJobs[MAX_KERN_Q_JOBS];
 
 /******************************************************************************
-* kernQLibInit - Initialize kernel work queue
-*
-* RETURNS: OK
-******************************************************************************/
+ * kernQLibInit - Initialize kernel work queue
+ *
+ * RETURNS: OK
+ */
 
-STATUS kernQLibInit(void)
+STATUS kernQLibInit(
+    void
+    )
 {
-  kernQReadIndex = 0;
-  kernQWriteIndex = 0;
-  kernQEmpty = TRUE;
+    kernQReadIndex = 0;
+    kernQWriteIndex = 0;
+    kernQEmpty = TRUE;
 
-  return(OK);
+    return OK;
 }
 
 /******************************************************************************
-* kernQAdd0 - Add a kernel job with no aguments
-*
-* RETURNS: N/A
-******************************************************************************/
+ * kernQAdd0 - Add a kernel job with no aguments
+ *
+ * RETURNS: N/A
+ */
 
-void kernQAdd0(FUNCPTR func)
+void kernQAdd0(
+    FUNCPTR func    /* ptr to deferred function */
+    )
 {
-  int oldLevel;
-  FAST KERN_JOB *pJob;
+    int oldLevel;                 /* previous interrupt lock level */
+    FAST KERN_JOB *pJob;          /* ptr to job item */
 
-  INT_LOCK(oldLevel);					/* LOCK INTERRUPTS */
+    INT_LOCK(oldLevel);           /* LOCK INTERRUPTS */
 
-  /* Get place to store job in buffer */
-  pJob = (KERN_JOB *) &kernJobs[kernQWriteIndex];
+    /* Get place to store job in buffer */
+    pJob = (KERN_JOB *) &kernJobs[kernQWriteIndex];
 
-  /* Increase pointer */
-  kernQWriteIndex++;
+    kernQWriteIndex++;                   /* Advance ring buffer write index */
+    kernQWriteIndex &= (MAX_KERN_Q_JOBS - 1);
 
-  if (kernQWriteIndex == kernQReadIndex)
-    kernQPanic();
-
-  INT_UNLOCK(oldLevel);					/* UNLOCK INTERRUPTS */
-
-  /* Queue not empty */
-  kernQEmpty = FALSE;
-
-  /* Store data */
-  pJob->func = func;
-  pJob->numArgs = 0;
-}
-
-/******************************************************************************
-* kernQAdd1 - Add a kernel job with one agument
-*
-* RETURNS: N/A
-******************************************************************************/
-
-void kernQAdd1(FUNCPTR func, ARG arg1)
-{
-  int oldLevel;
-  FAST KERN_JOB *pJob;
-
-  INT_LOCK(oldLevel);					/* LOCK INTERRUPTS */
-
-  /* Get place to store job in buffer */
-  pJob = (KERN_JOB *) &kernJobs[kernQWriteIndex];
-
-  /* Increase pointer */
-  kernQWriteIndex++;
-
-  if (kernQWriteIndex == kernQReadIndex)
-    kernQPanic();
-
-  INT_UNLOCK(oldLevel);					/* UNLOCK INTERRUPTS */
-
-  /* Queue not empty */
-  kernQEmpty = FALSE;
-
-  /* Store data */
-  pJob->func = func;
-  pJob->arg1 = arg1;
-  pJob->numArgs = 1;
-}
-
-/******************************************************************************
-* kernQAdd2 - Add a kernel job with two aguments
-*
-* RETURNS: N/A
-******************************************************************************/
-
-void kernQAdd2(FUNCPTR func,ARG arg1,ARG arg2)
-{
-  int oldLevel;
-  FAST KERN_JOB *pJob;
-
-  INT_LOCK(oldLevel);					/* LOCK INTERRUPTS */
-
-  /* Get place to store job in buffer */
-  pJob = (KERN_JOB *) &kernJobs[kernQWriteIndex];
-
-  /* Increase pointer */
-  kernQWriteIndex++;
-
-  if (kernQWriteIndex == kernQReadIndex)
-    kernQPanic();
-
-  INT_UNLOCK(oldLevel);					/* UNLOCK INTERRUPTS */
-
-  /* Queue not empty */
-  kernQEmpty = FALSE;
-
-  /* Store data */
-  pJob->func = func;
-  pJob->arg1 = arg1;
-  pJob->arg2 = arg2;
-  pJob->numArgs = 2;
-}
-
-/******************************************************************************
-* kernQDoWork - Do job on the kernel queue
-*
-* RETURNS: N/A
-******************************************************************************/
-
-void kernQDoWork(void)
-{
-  FAST KERN_JOB *pJob;
-  int keepErrno;
-
-  /* Store current errno */
-  keepErrno = errno;
-
-  /* Do all jobs on store */
-  while (kernQReadIndex != kernQWriteIndex) {
-
-    /* Get next job */
-    pJob = (KERN_JOB *) &kernJobs[kernQReadIndex];
-
-    /* Increase pointer */
-    kernQReadIndex++;
-
-    /* Call job procedure */
-    switch(pJob->numArgs) {
-      case 1 : (FUNCPTR *) (pJob->func)(pJob->arg1);
-	       break;
-      case 2 : (FUNCPTR *) (pJob->func)(pJob->arg1, pJob->arg2);
-	       break;
-      default: (FUNCPTR *) (pJob->func)();
-	       break;
+    if (kernQWriteIndex == kernQReadIndex)
+    {
+        kernQPanic();
     }
 
-  }
+    INT_UNLOCK(oldLevel);                /* UNLOCK INTERRUPTS */
 
-  /* Queue must be empty */
-  kernQEmpty = TRUE;
+    /* Queue not empty */
+    kernQEmpty = FALSE;
 
-  /* Restore errno */
-  errno = keepErrno;
+    /* Store data */
+    pJob->func = func;
+    pJob->numArgs = 0;
 }
 
 /******************************************************************************
-* kernQPanic - Fatal error in queue
-*
-* RETURNS: N/A
-******************************************************************************/
+ * kernQAdd1 - Add a kernel job with one agument
+ *
+ * RETURNS: N/A
+ */
 
-void kernQPanic(void)
+void kernQAdd1(
+    FUNCPTR func,     /* ptr to function to add */
+    ARG arg1          /* 1st argument to deferred function */
+    )
 {
-  sysReboot();
+    int oldLevel;
+    FAST KERN_JOB *pJob;
+
+    INT_LOCK(oldLevel);                  /* LOCK INTERRUPTS */
+
+    /* Get place to store job in buffer */
+    pJob = (KERN_JOB *) &kernJobs[kernQWriteIndex];
+
+    kernQWriteIndex++;                   /* Advance ring buffer write index */
+    kernQWriteIndex &= (MAX_KERN_Q_JOBS - 1);
+
+    if (kernQWriteIndex == kernQReadIndex)
+    {
+        kernQPanic();
+    }
+
+    INT_UNLOCK(oldLevel);                /* UNLOCK INTERRUPTS */
+
+    /* Queue not empty */
+    kernQEmpty = FALSE;
+
+    /* Store data */
+    pJob->func = func;
+    pJob->arg1 = arg1;
+    pJob->numArgs = 1;
+}
+
+/******************************************************************************
+ * kernQAdd2 - Add a kernel job with two aguments
+ *
+ * RETURNS: N/A
+ */
+
+void kernQAdd2(
+    FUNCPTR func,     /* ptr to function to add */
+    ARG arg1,         /* 1st argument to deferred function */
+    ARG arg2          /* 2nd argument to deferred function */
+    )
+{
+    int oldLevel;
+    FAST KERN_JOB *pJob;
+
+    INT_LOCK(oldLevel);                  /* LOCK INTERRUPTS */
+
+    /* Get place to store job in buffer */
+    pJob = (KERN_JOB *) &kernJobs[kernQWriteIndex];
+
+    kernQWriteIndex++;                   /* Advance ring buffer write index */
+    kernQWriteIndex &= (MAX_KERN_Q_JOBS - 1);
+
+    if (kernQWriteIndex == kernQReadIndex)
+    {
+        kernQPanic();
+    }
+
+    INT_UNLOCK(oldLevel);                /* UNLOCK INTERRUPTS */
+
+    /* Queue not empty */
+    kernQEmpty = FALSE;
+
+    /* Store data */
+    pJob->func = func;
+    pJob->arg1 = arg1;
+    pJob->arg2 = arg2;
+    pJob->numArgs = 2;
+}
+
+/******************************************************************************
+ * kernQDoWork - Do job on the kernel queue
+ *
+ * RETURNS: N/A
+ */
+
+void kernQDoWork(
+    void
+    )
+{
+    FAST KERN_JOB *pJob;
+    int keepErrno;
+    int level;
+
+    /* Store current errno */
+    keepErrno = errno;
+    INT_LOCK(level);
+
+    /*
+     * Do all jobs on store.
+     * As <kernelState> is TRUE, more jobs can still come in.  Thus, it
+     * is important to lock interrupts while mucking with the indices.
+     */
+
+    while (kernQReadIndex != kernQWriteIndex)
+    {
+        /* Get next job */
+        pJob = (KERN_JOB *) &kernJobs[kernQReadIndex];
+
+        kernQReadIndex++;                  /* Advance ring buffer read index */
+        kernQReadIndex &= (MAX_KERN_Q_JOBS - 1);
+
+        INT_UNLOCK(level);
+
+        /* Call job procedure (<kernelState> is TRUE!) */
+        switch(pJob->numArgs)
+        {
+            case 1:
+                (FUNCPTR *) (pJob->func)(pJob->arg1);
+                break;
+            case 2:
+                (FUNCPTR *) (pJob->func)(pJob->arg1, pJob->arg2);
+                break;
+            default:
+                (FUNCPTR *) (pJob->func)();
+                break;
+        }
+
+        INT_LOCK(level);   /* Lock interrupts again. */
+    }
+
+    kernQEmpty = TRUE;     /* Queue must be empty */
+    INT_UNLOCK(level);     /* Restore interrupts */
+
+    errno = keepErrno;     /* Restore errno */
+}
+
+/******************************************************************************
+ * kernQPanic - Fatal error in queue
+ *
+ * RETURNS: N/A
+ */
+
+void kernQPanic(
+    void
+    )
+{
+    sysReboot ();
 }
 
