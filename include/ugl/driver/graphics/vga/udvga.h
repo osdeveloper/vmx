f4f1ca434af0557bcc91f0306f22626679f4ee37
phoboz <alexuspol@hotmail.com>
2013-12-07 19:55:06 +0100

    Adpation to style guide.

--- include/ugl/driver/graphics/vga/udvga.h
+++ include/ugl/driver/graphics/vga/udvga.h
@@ -1,29 +1,29 @@
 /******************************************************************************
-*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-*
-*   This file is part of Real VMX.
-*   Copyright (C) 2008 - 2009 Surplus Users Ham Society
-*
-*   Real VMX is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU Lesser General Public License as published by
-*   the Free Software Foundation, either version 2.1 of the License, or
-*   (at your option) any later version.
-*
-*   Real VMX is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU Lesser General Public License for more details.
-*
-*   You should have received a copy of the GNU Lesser General Public License
-*   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
-******************************************************************************/
+ *   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *   This file is part of Real VMX.
+ *   Copyright (C) 2013 Surplus Users Ham Society
+ *
+ *   Real VMX is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Real VMX is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* udvga.h - Universal graphics library vga driver */
 
 #ifndef _udvga_h
 #define _udvga_h
 
-#define UGL_GRAPHICS_CREATE		uglVgaDevCreate
+#define UGL_GRAPHICS_CREATE             uglVgaDevCreate
 
 #ifndef _ASMLANGUAGE
 
@@ -35,71 +35,83 @@ extern "C" {
 #include <ugl/driver/graphics/generic/udgen.h>
 
 typedef struct ugl_vga_driver {
-  UGL_GENERIC_DRIVER generic;		/* Generic driver (required) */
-  UGL_SIZE colorPlanes;			/* Number of planes */
-  UGL_SIZE bytesPerLine;		/* Number of bytes per line */
-  UGL_RASTER_OP rasterOp;		/* Raster operation */
-  UGL_COLOR color;			/* Current color */
+    UGL_GENERIC_DRIVER  generic;        /* Generic driver (required) */
+    UGL_SIZE            colorPlanes;    /* Number of planes */
+    UGL_SIZE            bytesPerLine;   /* Number of bytes per line */
+    UGL_RASTER_OP       rasterOp;       /* Raster operation */
+    UGL_COLOR           color;          /* Current color */
 } UGL_VGA_DRIVER;
 
 typedef struct ugl_vga_ddb {
-  UGL_BMAP_HEADER header;		/* Bitmap header */
-  UGL_UINT16 colorDepth;		/* Color depth */
-  UGL_UINT16 stride;			/* Scanline offset */
-  UGL_UINT16 shiftValue;		/* Alignment shift */
-  UGL_UINT8 **pPlaneArray;		/* Bit planes */
+    UGL_BMAP_HEADER  header;            /* Bitmap header */
+    UGL_UINT16       colorDepth;        /* Color depth */
+    UGL_UINT16       stride;            /* Scanline offset */
+    UGL_UINT16       shiftValue;        /* Alignment shift */
+    UGL_UINT8 **     pPlaneArray;       /* Bit planes */
 } UGL_VGA_DDB;
 
 typedef struct ugl_vga_mddb {
-  UGL_BMAP_HEADER header;		/* Bitmap header */
-  UGL_UINT16 stride;			/* Scanline offset */
-  UGL_UINT16 shiftValue;		/* Alignment shift */
-  UGL_UINT8 **pPlaneArray;		/* Bit planes */
+  UGL_BMAP_HEADER  header;              /* Bitmap header */
+  UGL_UINT16       stride;              /* Scanline offset */
+  UGL_UINT16       shiftValue;          /* Alignment shift */
+  UGL_UINT8 **     pPlaneArray;         /* Bit planes */
 } UGL_VGA_MDDB;
 
-extern UGL_UGI_DRIVER *uglVgaDevCreate(UGL_UINT32 arg0,
-				       UGL_UINT32 arg1,
-				       UGL_UINT32 arg2);
+/* Device initialization support functions */
+
+/******************************************************************************
+ *
+ * uglVgaDevCreate - Create vga graphics driver
+ *
+ * RETURNS: Pointer to driver structure (UGL_UGI_DRIVER *) or UGL_NULL
+ */
+
+UGL_UGI_DRIVER * uglVgaDevCreate (
+    UGL_UINT32  arg0,
+    UGL_UINT32  arg1,
+    UGL_UINT32  arg2
+    );
+
 extern UGL_STATUS uglVgaDevDestroy(UGL_DEVICE_ID devId);
 extern UGL_STATUS uglVgaInfo(UGL_DEVICE_ID devId,
-		      	     UGL_INFO_REQ infoReq,
-		      	     void *info);
+                             UGL_INFO_REQ infoReq,
+                             void *info);
 extern UGL_STATUS uglVgaGcSet(UGL_DEVICE_ID devId, UGL_GC_ID gc);
 extern UGL_STATUS uglVgaPixelSet(UGL_DEVICE_ID devId,
-			  	 UGL_POINT *p,
-			  	 UGL_COLOR c);
+                                 UGL_POINT *p,
+                                 UGL_COLOR c);
 extern UGL_STATUS uglVgaClutSet(UGL_DEVICE_ID devId,
-			 	UGL_ORD offset,
-			 	UGL_ARGB *pColors,
-			 	UGL_SIZE numColors);
+                                UGL_ORD offset,
+                                UGL_ARGB *pColors,
+                                UGL_SIZE numColors);
 extern UGL_STATUS uglVgaClutGet(UGL_DEVICE_ID devId,
-			        UGL_ORD offset,
-			        UGL_ARGB *pColors,
-			        UGL_SIZE numColors);
+                                UGL_ORD offset,
+                                UGL_ARGB *pColors,
+                                UGL_SIZE numColors);
 extern UGL_STATUS uglVga4BitColorConvert(UGL_DEVICE_ID devId,
-				  	 void *srcArray,
-				  	 UGL_COLOR_FORMAT srcFormat,
-				  	 void *destArray,
-				  	 UGL_COLOR_FORMAT destFormat,
-				  	 UGL_SIZE arraySize);
+                                         void *srcArray,
+                                         UGL_COLOR_FORMAT srcFormat,
+                                         void *destArray,
+                                         UGL_COLOR_FORMAT destFormat,
+                                         UGL_SIZE arraySize);
 extern UGL_DDB_ID uglVgaBitmapCreate(UGL_DEVICE_ID devId,
-			    	     UGL_DIB *pDib,
-			      	     UGL_DIB_CREATE_MODE createMode,
-			      	     UGL_UINT32 initValue,
-			      	     UGL_MEM_POOL_ID poolId);
+                                     UGL_DIB *pDib,
+                                     UGL_DIB_CREATE_MODE createMode,
+                                     UGL_UINT32 initValue,
+                                     UGL_MEM_POOL_ID poolId);
 extern UGL_STATUS uglVgaBitmapDestroy(UGL_DEVICE_ID devId,
-			       	      UGL_DDB_ID ddbId,
-			       	      UGL_MEM_POOL_ID poolId);
+                                      UGL_DDB_ID ddbId,
+                                      UGL_MEM_POOL_ID poolId);
 extern UGL_STATUS uglVgaBitmapBlt(UGL_DEVICE_ID devId,
-			          UGL_DDB_ID srcBmpId,
-			          UGL_RECT *pSrcRect,
-			          UGL_DDB_ID destBmpId,
-			          UGL_POINT *pDestPoint);
+                                  UGL_DDB_ID srcBmpId,
+                                  UGL_RECT *pSrcRect,
+                                  UGL_DDB_ID destBmpId,
+                                  UGL_POINT *pDestPoint);
 extern UGL_STATUS uglVgaBitmapWrite(UGL_DEVICE_ID devId,
-			     	    UGL_DIB *pDib,
-			     	    UGL_RECT *pSrcRect,
-			     	    UGL_DDB_ID ddbId,
-			     	    UGL_POINT *pDestPoint);
+                                    UGL_DIB *pDib,
+                                    UGL_RECT *pSrcRect,
+                                    UGL_DDB_ID ddbId,
+                                    UGL_POINT *pDestPoint);
 
 #ifdef __cplusplus
 }
--- src/ugl/driver/graphics/vga/udvga.c
+++ src/ugl/driver/graphics/vga/udvga.c
@@ -1,29 +1,28 @@
 /******************************************************************************
-*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-*
-*   This file is part of Real VMX.
-*   Copyright (C) 2008 - 2009 Surplus Users Ham Society
-*
-*   Real VMX is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU Lesser General Public License as published by
-*   the Free Software Foundation, either version 2.1 of the License, or
-*   (at your option) any later version.
-*
-*   Real VMX is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU Lesser General Public License for more details.
-*
-*   You should have received a copy of the GNU Lesser General Public License
-*   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
-******************************************************************************/
+ *   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *   This file is part of Real VMX.
+ *   Copyright (C) 2013 Surplus Users Ham Society
+ *
+ *   Real VMX is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Real VMX is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Real VMX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* udvga.c - Universal graphics library display driver for vga */
 
 #include <stdlib.h>
 #include <string.h>
 #include <arch/sysArchLib.h>
-
 
 #include <ugl/ugl.h>
 #include <ugl/driver/graphics/generic/udgen.h>
@@ -33,232 +32,254 @@
 
 /* Locals */
 UGL_LOCAL UGL_MODE modes[] = {
-  {"320x200x8 @ 60Hz",
-   320,200,8,60,
-   UGL_MODE_CRT,
-   UGL_MODE_INDEXED_COLOR,
-   0,
-   640,664,760,800,0,
-   480,491,493,525},
-  {"640x480x4 @ 60Hz",		/* h: 31.5 kHz v: 60Hz */
-   640,480,4,60,
-   UGL_MODE_CRT,
-   UGL_MODE_INDEXED_COLOR,
-   0,
-   640,664,760,800,0,
-   480,491,493,525},
+    {
+        "320x200x8 @ 60Hz",
+        320, 200, 8, 60,
+        UGL_MODE_CRT,
+        UGL_MODE_INDEXED_COLOR,
+        0,
+        640, 664, 760, 800, 0,
+        480, 491, 493, 525
+    },
+
+    {
+        "640x480x4 @ 60Hz",          /* h: 31.5 kHz v: 60Hz */
+        640, 480, 4, 60,
+        UGL_MODE_CRT,
+        UGL_MODE_INDEXED_COLOR,
+        0,
+        640, 664, 760, 800, 0,
+        480, 491, 493, 525
+    }
 };
 
 UGL_LOCAL UGL_MODE *pModes = modes;
 
-LOCAL UGL_STATUS uglVgaModeAvailGet(UGL_DEVICE_ID devId,
-			            UGL_UINT32 *pNumModes,
-			            const UGL_MODE **pModeArray);
-LOCAL UGL_STATUS uglVgaModeSet(UGL_DEVICE_ID devId, UGL_MODE *pMode);
+UGL_LOCAL UGL_STATUS uglVgaModeAvailGet (
+    UGL_DEVICE_ID     devId,
+    UGL_UINT32 *      pNumModes,
+    const UGL_MODE ** pModeArray
+    );
 
-/*******************************************************************************
+UGL_LOCAL UGL_STATUS uglVgaModeSet (
+    UGL_DEVICE_ID  devId,
+    UGL_MODE *     pMode
+    );
+
+/******************************************************************************
+ *
  * uglVgaDevCreate - Create vga graphics driver
  *
  * RETURNS: Pointer to driver structure (UGL_UGI_DRIVER *) or UGL_NULL
- ******************************************************************************/
+ */
 
-UGL_UGI_DRIVER *uglVgaDevCreate(UGL_UINT32 arg0,
-				UGL_UINT32 arg1,
-				UGL_UINT32 arg2)
-{
-  UGL_VGA_DRIVER *pDrv;
-  UGL_DEVICE_ID devId = UGL_NULL;
+UGL_UGI_DRIVER * uglVgaDevCreate (
+    UGL_UINT32  arg0,
+    UGL_UINT32  arg1,
+    UGL_UINT32  arg2
+    ) {
+    UGL_VGA_DRIVER * pDrv;
+    UGL_DEVICE_ID    devId = UGL_NULL;
 
-  /* Allocate memory for driver data structure */
-  pDrv = (UGL_VGA_DRIVER *) malloc( sizeof(UGL_VGA_DRIVER) );
-  if (pDrv == NULL)
-    return UGL_NULL;
+    /* Allocate memory for driver data structure */
+    pDrv = (UGL_VGA_DRIVER *) malloc (sizeof (UGL_VGA_DRIVER));
+    if (pDrv == NULL) {
+        return (UGL_NULL);
+    }
 
-  memset(pDrv, 0, sizeof(UGL_VGA_DRIVER));
+    /* Important to clear struct */
+    memset (pDrv, 0, sizeof (UGL_VGA_DRIVER));
 
-  /* Store at begining of device struct, since it must be the first field */
-  devId = (UGL_DEVICE_ID) pDrv;
+    /* Store at begining of device struct, since it must be the first field */
+    devId = (UGL_DEVICE_ID) pDrv;
 
-  /* Initialize device driver */
-  uglUgiDevInit(devId);
+    /* Initialize device driver */
+    uglUgiDevInit (devId);
 
-  /* Set frame buffer address */
-  pDrv->generic.fbAddress	=	(UGL_UINT8 *) 0xa0000L;
+    /* Set frame buffer address */
+    pDrv->generic.fbAddress = (UGL_UINT8 *) 0xa0000L;
 
-  /* Setup device support methods */
-  devId->destroy		=	uglVgaDevDestroy;
+    /* Setup device support methods */
+    devId->destroy          = uglVgaDevDestroy;
 
-  /* Setup info method */
-  devId->info			=	uglVgaInfo;
+    /* Setup info method */
+    devId->info             = uglVgaInfo;
 
-  /* Setup mode support methods */
-  devId->modeAvailGet		=	uglVgaModeAvailGet;
-  devId->modeSet		=	uglVgaModeSet;
+    /* Setup mode support methods */
+    devId->modeAvailGet     = uglVgaModeAvailGet;
+    devId->modeSet          = uglVgaModeSet;
 
-  /* Setup graphics context support methods */
-  devId->gcCreate		= 	uglGenericGcCreate;
-  devId->gcCopy			=	uglGenericGcCopy;
-  devId->gcDestroy		=	uglGenericGcDestroy;
-  devId->gcSet			=	uglVgaGcSet;
+    /* Setup graphics context support methods */
+    devId->gcCreate         = uglGenericGcCreate;
+    devId->gcCopy           = uglGenericGcCopy;
+    devId->gcDestroy        = uglGenericGcDestroy;
+    devId->gcSet            = uglVgaGcSet;
 
-  /* Set color support methods */
-  devId->colorAlloc		=	uglGenericColorAllocIndexed;
-  devId->colorFree		=	uglGenericColorFreeIndexed;
-  devId->clutGet		=	uglVgaClutGet;
-  devId->clutSet		=	uglVgaClutSet;
-  devId->colorConvert		=	uglVga4BitColorConvert;
+    /* Set color support methods */
+    devId->colorAlloc       = uglGenericColorAllocIndexed;
+    devId->colorFree        = uglGenericColorFreeIndexed;
+    devId->clutGet          = uglVgaClutGet;
+    devId->clutSet          = uglVgaClutSet;
+    devId->colorConvert     = uglVga4BitColorConvert;
 
-  return devId;
+    return (devId);
 }
 
-/*******************************************************************************
+/******************************************************************************
+ *
  * uglVgaModeAvailGet - Get available graphics modes
  *
  * RETURNS: UGL_STATUS_OK
- ******************************************************************************/
+ */
 
-UGL_STATUS uglVgaModeAvailGet(UGL_DEVICE_ID devId,
-			      UGL_UINT32 *pNumModes,
-			      const UGL_MODE **pModeArray)
-{
-  /* Store pointer to list */
-  *pModeArray = pModes;
+UGL_LOCAL UGL_STATUS uglVgaModeAvailGet (
+    UGL_DEVICE_ID     devId,
+    UGL_UINT32 *      pNumModes,
+    const UGL_MODE ** pModeArray
+    ) {
 
-  /* Calculate number of elements */
-  *pNumModes = sizeof(modes) / sizeof(modes[0]);
+    *pModeArray = pModes;
 
-  return UGL_STATUS_OK;
+    /* Calculate number of elements */
+    *pNumModes = sizeof (modes) / sizeof (modes[0]);
+
+    return (UGL_STATUS_OK);
 }
 
-/*******************************************************************************
+/******************************************************************************
+ *
  * uglVgaModeSet - Set graphics mode
  *
  * RETURNS: UGL_STATUS_OK or UGL_STATUS_ERROR
- ******************************************************************************/
+ */
 
-UGL_STATUS uglVgaModeSet(UGL_DEVICE_ID devId, UGL_MODE *pMode)
-{
-  UGL_VGA_DRIVER *pDrv;
-  UGL_INT32 modeIndex;
-  UGL_UINT8 byteValue;
-  struct vgaHWRec regs, oldRegs;
+UGL_LOCAL UGL_STATUS uglVgaModeSet (
+    UGL_DEVICE_ID  devId,
+    UGL_MODE *     pMode
+    ) {
+    UGL_VGA_DRIVER * pDrv;
+    UGL_INT32        modeIndex;
+    UGL_UINT8        byteValue;
+    struct vgaHWRec  regs;
 
-  /* Get driver from device struct, since it it the first entry */
-  pDrv = (UGL_VGA_DRIVER *) devId;
+    /* Get driver from device struct, since it it the first entry */
+    pDrv = (UGL_VGA_DRIVER *) devId;
 
-  /* Get requested mode from list */
-  modeIndex = uglGenericModeFind(modes, pMode,
-				 sizeof(modes) / sizeof(modes[0]));
-  if (modeIndex == UGL_STATUS_ERROR)
-    return UGL_STATUS_ERROR;
+    /* Get requested mode from list */
+    modeIndex = uglGenericModeFind (modes, pMode,
+                                    sizeof (modes) / sizeof (modes[0]));
+    if (modeIndex == UGL_STATUS_ERROR) {
+        return (UGL_STATUS_ERROR);
+    }
 
-  /* Store current mode */
-  devId->pMode = &modes[modeIndex];
+    /* Store current mode */
+    devId->pMode = &modes[modeIndex];
 
-  /* Set graphics mode */
-  vgaInitMode(&modes[modeIndex], &regs);
-  vgaLoadPalette(&regs, NULL);
-  vgaRestore(&regs, 0);
+    /* Set graphics mode */
+    vgaInitMode (&modes[modeIndex], &regs);
+    vgaLoadPalette (&regs, NULL);
+    vgaRestore (&regs, 0);
 
-  switch(modeIndex) {
+    /* Select graphics mode */
+    switch(modeIndex) {
+        /* Vga mode 13 */
+        case 0:
+            /* Change sync polarity */
+            UGL_OUT_BYTE (0x3c0, 0x63);
 
-    case 0:
+            /* Modify sequencer registers */
+            UGL_OUT_WORD (0x3c4, 0x0e04);       /* Enable Chain-4 */
 
-    /* Modify registers */
-    /* Change sync polarity */
-    UGL_OUT_BYTE(0x3c0, 0x63);
+            /* Modify graphics controller registers */
+            UGL_OUT_WORD (0x3ce, 0x4005);       /* Set mode register */
 
-    /* Modify sequencer registers */
-    UGL_OUT_WORD(0x3c4, 0x0e04);		/* Enable Chain-4 */
+            /* Modify CRT controller registers */
+            UGL_OUT_WORD (0x3d4, 0x2c11);       /* Disable write protection */
+            UGL_OUT_WORD (0x3d4, 0xbf06);       /* Vertical total */
+            UGL_OUT_WORD (0x3d4, 0x1f07);       /* Overflow register */
+            UGL_OUT_WORD (0x3d4, 0x4109);       /* Set duplicate scanlines */
+            UGL_OUT_WORD (0x3d4, 0x9c10);       /* Vertical retrace start */
+            UGL_OUT_WORD (0x3d4, 0x8e11);       /* Vertical retrace end */
+            UGL_OUT_WORD (0x3d4, 0x8f12);       /* Vertical display end */
+            UGL_OUT_WORD (0x3d4, 0x2813);       /* Set logical width */
+            UGL_OUT_WORD (0x3d4, 0x4014);       /* Enable double word mode */
+            UGL_OUT_WORD (0x3d4, 0x9615);       /* Vertical blanking start */
+            UGL_OUT_WORD (0x3d4, 0xb916);       /* Vertical blanking end */
+            UGL_OUT_WORD (0x3d4, 0xa317);       /* Enable word mode */
 
-    /* Modify graphics controller registers */
-    UGL_OUT_WORD(0x3ce, 0x4005);		/* Set mode register */
+            /* Set attribute registers */
+            byteValue = UGL_IN_BYTE(0x3da);     /* Start attribute set */
+            UGL_OUT_BYTE (0x3c0, 0x10);         /* Set mode control register */
+            UGL_OUT_BYTE (0x3c0, 0x41);         /* for lores 256 color */
+            UGL_OUT_BYTE (0x3c0, 0x20);         /* End attribute set */
 
-    /* Modify CRT controller registers */
-    UGL_OUT_WORD(0x3d4, 0x2c11);		/* Disable write protection */
-    UGL_OUT_WORD(0x3d4, 0xbf06);		/* Vertical total */
-    UGL_OUT_WORD(0x3d4, 0x1f07);		/* Overflow register */
-    UGL_OUT_WORD(0x3d4, 0x4109);		/* Set duplicate scanlines */
-    UGL_OUT_WORD(0x3d4, 0x9c10);		/* Vertical retrace start */
-    UGL_OUT_WORD(0x3d4, 0x8e11);		/* Vertical retrace end */
-    UGL_OUT_WORD(0x3d4, 0x8f12);		/* Vertical display end */
-    UGL_OUT_WORD(0x3d4, 0x2813);		/* Set logical width */
-    UGL_OUT_WORD(0x3d4, 0x4014);		/* Enable double word mode */
-    UGL_OUT_WORD(0x3d4, 0x9615);		/* Vertical blanking start */
-    UGL_OUT_WORD(0x3d4, 0xb916);		/* Vertical blanking end */
-    UGL_OUT_WORD(0x3d4, 0xa317);		/* Enable word mode */
+            /* Set releated info */
+            pDrv->bytesPerLine   = devId->pMode->width;
+            pDrv->colorPlanes    = devId->pMode->depth;
 
-    /* Set attribute registers */
-    byteValue = UGL_IN_BYTE(0x3da);	/* Start attribute set */
-    UGL_OUT_BYTE(0x3c0, 0x10);		/* Set mode control register */
-    UGL_OUT_BYTE(0x3c0, 0x41);		/* for lores 256 color */
-    UGL_OUT_BYTE(0x3c0, 0x20);		/* End attribute set */
+            /* Set graphics primitives support methods */
+            devId->pixelSet      = uglGeneric8BitPixelSet;
 
-    /* Set releated info */
-    pDrv->bytesPerLine	=	devId->pMode->width;
-    pDrv->colorPlanes	=	devId->pMode->depth;
+            /* Setup bitmap support methods */
+            devId->bitmapCreate  = uglGeneric8BitBitmapCreate;
+            devId->bitmapDestroy = uglGeneric8BitBitmapDestroy;
+            devId->bitmapBlt     = uglGeneric8BitBitmapBlt;
+            devId->bitmapWrite   = uglGeneric8BitBitmapWrite;
 
-    /* Set graphics primitives support methods */
-    devId->pixelSet		=	uglGeneric8BitPixelSet;
+            /* Create palette for 256 colors */
+            if (uglGenericClutCreate ((UGL_GENERIC_DRIVER *) devId, 256)
+                != UGL_STATUS_OK) {
+                return (UGL_STATUS_ERROR);
+            }
 
-    /* Setup bitmap support methods */
-    devId->bitmapCreate		=	uglGeneric8BitBitmapCreate;
-    devId->bitmapDestroy	=	uglGeneric8BitBitmapDestroy;
-    devId->bitmapBlt		=	uglGeneric8BitBitmapBlt;
-    devId->bitmapWrite		=	uglGeneric8BitBitmapWrite;
+            /* Clear screen */
+            memset (pDrv->generic.fbAddress, 0,
+                    devId->pMode->height * pDrv->bytesPerLine);
+            break;
 
-    /* Create palette for 256 colors */
-    if (uglGenericClutCreate((UGL_GENERIC_DRIVER *) devId, 256) != UGL_STATUS_OK)
-      return UGL_STATUS_ERROR;
+        /* Vga mode 12 */
+        case 1:
+            /* Set releated info */
+            pDrv->bytesPerLine   = devId->pMode->width / 8;
+            pDrv->colorPlanes    = devId->pMode->depth;
 
-    /* Clear screen */
-    memset(pDrv->generic.fbAddress, 0,
-	   devId->pMode->height * pDrv->bytesPerLine);
+            /* Set graphics primitives support methods */
+            devId->pixelSet      = uglVgaPixelSet;
 
-    break;
+            /* Setup bitmap support methods */
+            devId->bitmapCreate  = uglVgaBitmapCreate;
+            devId->bitmapDestroy = uglVgaBitmapDestroy;
+            devId->bitmapBlt     = uglVgaBitmapBlt;
+            devId->bitmapWrite   = uglVgaBitmapWrite;
 
-    case 1:
+            /* Create palette for 16 colors */
+            if (uglGenericClutCreate ((UGL_GENERIC_DRIVER *) devId, 16)
+                != UGL_STATUS_OK) {
+                return (UGL_STATUS_ERROR);
+            }
 
-    /* Set releated info */
-    pDrv->bytesPerLine	=	devId->pMode->width / 8;
-    pDrv->colorPlanes	=	devId->pMode->depth;
+            /* Set write mode 3 */
+            UGL_OUT_BYTE (0x3ce, 0x05);
+            byteValue = UGL_IN_BYTE (0x3cf);
+            UGL_OUT_BYTE (0x3cf, byteValue | 0x03);
 
-    /* Set graphics primitives support methods */
-    devId->pixelSet		=	uglVgaPixelSet;
+            /* Set set/reset register */
+            UGL_OUT_WORD (0x3ce, 0x0f01);
 
-    /* Setup bitmap support methods */
-    devId->bitmapCreate		=	uglVgaBitmapCreate;
-    devId->bitmapDestroy	=	uglVgaBitmapDestroy;
-    devId->bitmapBlt		=	uglVgaBitmapBlt;
-    devId->bitmapWrite		=	uglVgaBitmapWrite;
+            /* Clear screen */
+            UGL_OUT_WORD (0x3ce, 0);
+            memset (pDrv->generic.fbAddress, 0xff,
+                    devId->pMode->height * pDrv->bytesPerLine);
 
-    /* Create palette for 16 colors */
-    if (uglGenericClutCreate((UGL_GENERIC_DRIVER *) devId, 16) != UGL_STATUS_OK)
-      return UGL_STATUS_ERROR;
+            /* Enable video output */
+            UGL_OUT_BYTE (0x3c4, 0x01);
+            UGL_OUT_BYTE (0x3c5, 0x01);
+            break;
 
-    /* Set write mode 3 */
-    UGL_OUT_BYTE(0x3ce, 0x05);
-    byteValue = UGL_IN_BYTE(0x3cf);
-    UGL_OUT_BYTE(0x3cf, byteValue | 0x03);
+        default:
+            return (UGL_STATUS_ERROR);
+    }
 
-    /* Set set/reset register */
-    UGL_OUT_WORD(0x3ce, 0x0f01);
-
-    /* Clear screen */
-    UGL_OUT_WORD(0x3ce, 0);
-    memset(pDrv->generic.fbAddress, 0xff,
-	   devId->pMode->height * pDrv->bytesPerLine);
-
-    /* Enable video output */
-    UGL_OUT_BYTE(0x3c4, 0x01);
-    UGL_OUT_BYTE(0x3c5, 0x01);
-
-    break;
-
-    default:
-    return UGL_STATUS_ERROR;
-  }
-
-  return UGL_STATUS_OK;
+    return (UGL_STATUS_OK);
 }
 
